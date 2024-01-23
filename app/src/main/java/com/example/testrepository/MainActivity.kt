b504b615a0c85f9578ffbf91100517a3b1cb66e4
prateek.arora <prateek.arora@impressico.com>
2024-01-23 18:57:22 +0530

    Change surface modifier to wrap content

--- app/src/main/java/com/example/testrepository/MainActivity.kt
+++ app/src/main/java/com/example/testrepository/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
             TestRepositoryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.wrapContentSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Hi Prateek")
